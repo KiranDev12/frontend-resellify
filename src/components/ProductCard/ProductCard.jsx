f1467859e4bb1dd7b51ab812b1f04e38258a270b
Kiran Dev K <kirandev.edu@gmail.com>
2023-11-23 18:42:50 +0530

    products-add

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,34 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "swiper/css";
 import "./ProductCard.css";
-// import data from "../../utils/slider.json";
 import { sliderSettings } from "../../utils/common";
-import { useState } from "react";
+import loginImg from "../../assets/login-img.jpg";
 function ProductCard() {
   const [data, setData] = useState([]);
-  //   [
-  //     {
-  //         "product_id": 123,
-  //         "product_name": "Necklace",
-  //         "product_desc": "Gold",
-  //         "product_date": "2020-05-07",
-  //         "product_rating": 4.2,
-  //         "product_life": 2,
-  //         "product_price": 999,
-  //         "product_img": null,
-  //         "merchant_id": 1445
-  //     }
-  fetch("http://127.0.0.1:8000/fetch/products/")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      return setData(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/fetch/products/")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false); // Set loading to false when data is available
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false); // Set loading to false in case of an error
+      });
+  }, []); // The empty dependency array ensures the effect runs only once after the initial render.
+
   return (
     <section className="r-wrapper">
       <div className="paddings innerWidth r-container">
@@ -36,28 +28,33 @@ function ProductCard() {
           <span className="orangeText">Best Choices</span>
           <span className="primaryText">Popular Products</span>
         </div>
-        <Swiper {...sliderSettings}>
-          <SliderButtons />
-          {data.map((card, i) => (
-            <SwiperSlide key={i}>
-              <div className="flexColStart r-card">
-                <img src={card.product_img} alt="" />
-                <span className="secondaryText r-price">
-                  <span style={{ color: "orange" }}>$</span>
-                  <span>{card.product_price}</span>
-                </span>
-                <span className="primaryText">{card.product_name}</span>
-                <div className="span secondaryText">{card.product_desc}</div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        {loading ? (
+          // Display a loading message while data is being fetched
+          <p>Loading...</p>
+        ) : (
+          <Swiper {...sliderSettings}>
+            <SliderButtons />
+            {data.map((card, i) => (
+              <SwiperSlide key={i}>
+                <div className="flexColStart r-card">
+                  <img src={loginImg} alt="" />
+                  <span className="secondaryText r-price">
+                    <span style={{ color: "orange" }}>$</span>
+                    <span>{card.product_price}</span>
+                  </span>
+                  <span className="primaryText">{card.product_name}</span>
+                  <div className="span secondaryText">{card.product_rating}</div>
+                  
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </section>
   );
 }
 
-export default ProductCard;
 const SliderButtons = () => {
   const swiper = useSwiper();
 
@@ -68,3 +65,5 @@ const SliderButtons = () => {
     </div>
   );
 };
+
+export default ProductCard;
--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-
+import hero from "../../assets/hero.jpg"
 function Products() {
-
   return (
     <div>
-      
+      <div className="flexColStart r-card">
+        <img src={hero} alt="" />
+        <span className="secondaryText r-price">
+          <span style={{ color: "orange" }}>$</span>
+          <span>900</span>
+        </span>
+        <span className="primaryText">Jewel</span>
+        <div className="span secondaryText">Gold</div>
+      </div>
     </div>
   );
 }
